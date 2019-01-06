7f0d196a2a0542b1714bfa087f1e6fa340b212d8
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2019-01-06 19:46:20 +0500

    Simplified Linux compilation script

--- build_linux_release.sh
+++ build_linux_release.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-msbuild ./Sonic4_ModLoader/AMBPatcher/AMBPatcher.csproj /p:Configuration=Release
-msbuild ./Sonic4_ModLoader/ManagerLauncher/ManagerLauncher.csproj /p:Configuration=Release
-msbuild ./Sonic4_ModLoader/PatchLauncher/PatchLauncher.csproj /p:Configuration=Release
-msbuild ./Sonic4_ModLoader/Sonic4ModManager/Sonic4ModManager.csproj /p:Configuration=Release
+msbuild ./Sonic4_ModLoader/Sonic4_ModLoader.sln /p:Configuration=Release
