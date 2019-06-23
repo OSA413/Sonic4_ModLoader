1313896416704836f659395aa11c7cbd6917d921
OSA413 <OSA413@users.noreply.github.com>
2019-06-23 21:21:43 +0500

    Removed the only useful script since AMBPatcher now can do this,

--- unit-tests/AMBPatcher/update_from_release.py
+++ unit-tests/AMBPatcher/update_from_release.py
@@ -2,5 +2,5 @@ import shutil, os, sys
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
-shutil.copyfile("../../../dist/Sonic4ModLoader/AMBPatcher.exe",  "sandbox/AMBPatcher.exe")
+shutil.copyfile("../../dist/Sonic4ModLoader/AMBPatcher.exe",  "sandbox/AMBPatcher.exe")
 print("Updated from release!\n")
