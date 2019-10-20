b575e00dca8696bce63f114a450a6375c8c2d34f
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2019-10-20 19:28:25 +0500

    Added script that will update dependencies automatically

---
+++ update_dependencies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#please, keep all sh scripts in Unix new line (LF)
+cd "$(dirname "$0")"
+
+rm -r "dependencies_source"
+mkdir "dependencies_source"
+cd "dependencies_source"
+
+
+#SonicAudioTools
+git clone --depth=1 https://github.com/blueskythlikesclouds/SonicAudioTools
+cd "SonicAudioTools"
+nuget restore SonicAudioTools.sln
+msbuild SonicAudioTools.sln /p:Configuration=Release
+
+cp ./Release/CsbEditor.exe ./../../dependencies/SonicAudioTools/CsbEditor.exe
+cp ./Release/SonicAudioLib.dll ./../../dependencies/SonicAudioTools/SonicAudioLib.dll
+cp ./LICENSE.md ./../../dependencies/SonicAudioTools/LICENSE
+
+cd ..
+
+#7-Zip
+mkdir "7-Zip"
+cd "7-Zip"
+url=$(curl -LIs -w %{url_effective} -o /dev/null https://sourceforge.net/projects/sevenzip/files/latest/download)
+version=$(basename $(dirname $url))
+curl  $url > 7z_install.exe
+echo https://sourceforge.net/projects/sevenzip/files/7-Zip/$version > ./../../dependencies/7-Zip/link
+
+7z x 7z_install.exe
+
+cp 7z.exe       ./../../dependencies/7-Zip/7z.exe
+cp 7z.dll       ./../../dependencies/7-Zip/7z.dll
+cp License.txt  ./../../dependencies/7-Zip/License.txt
