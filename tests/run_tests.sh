68d121b44da0d534d40c76fc484860b11f7213e3
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2020-07-24 05:59:31 +0500

    Now it will be shown when tests are failed

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,10 +4,17 @@ cd "$(dirname "$0")"
 echo
 echo "Running tests..."
 
+EXIT_CODE=0
+
 for dir in $(ls ./); do
     [ ! -d $dir ] && continue
     [ $dir == "__pycache__" ] && continue
     echo
     echo "$dir"
     python3 $dir"/run.py"
+    
+    [ "$?" != "0" ] && EXIT_CODE=1
+
 done
+
+exit $EXIT_CODE
