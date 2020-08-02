0e4bbfbd76ad80a06ee1b442db0679a7fceb80b7
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2020-08-02 23:16:04 +0500

    Returned time into tests

--- tests/AMBPatcher/instructions.py
+++ tests/AMBPatcher/instructions.py
@@ -5,15 +5,15 @@ def get_test(test_name="", AMBPATCHER="", MAIN_AMB=""):
         ".add:files/3": [[AMBPATCHER, "add", MAIN_AMB, "files/3"]],
         ".swap_endianness": [[AMBPATCHER, "swap_endianness", MAIN_AMB]],
         ".extract": [[AMBPATCHER, "extract", MAIN_AMB]],
-        
+
         "create": [[AMBPATCHER, "create", MAIN_AMB]],
-        "add": ["create", ".add:files/1", ".add:files/2", ".add:files/3"],
-        "swap_endianness": ["add", ".swap_endianness"],
-        "swap_endianness_x2": ["swap_endianness", ".swap_endianness"],
+        "add": ["create", ".add:files/1", ".add:files/2", "#TIME", ".add:files/3"],
+        "swap_endianness": ["add", "#TIME",".swap_endianness"],
+        "swap_endianness_x2": ["swap_endianness", "#TIME", ".swap_endianness"],
         "add_to_swapped": ["create", ".swap_endianness",
-                            ".add:files/1", ".add:files/2", ".add:files/3"],
-        "extract": ["add", ".extract"],
-        "extract_swapped": ["add_to_swapped", ".extract"]
+                            ".add:files/1", ".add:files/2","#TIME", ".add:files/3"],
+        "extract": ["add","#TIME", ".extract"],
+        "extract_swapped": ["add_to_swapped","#TIME", ".extract"]
     }
     
     if test_name == "":
--- tests/AMBPatcher/run.py
+++ tests/AMBPatcher/run.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 cwd = os.path.dirname(sys.argv[0])
 if cwd != "":
@@ -18,12 +19,19 @@ def run_test(test_name):
     test_sequence = instructions.get_test(test_name=test_name,
         AMBPATCHER = PATHS[AMBPATCHER], MAIN_AMB = "sandbox/"+MAIN_AMB)
 
+    test_time = time.time()
+
     for seq in test_sequence:
         if type(seq) == str:
-            run_test(seq)
+            if seq == "#TIME":
+                test_time = time.time()
+            else:
+                run_test(seq)
         else:
             if MONO: seq.insert(0, MONO)
             subprocess.check_output(seq)
+
+    return time.time() - test_time
 
 def check_files(test_name, REBUILD_SHA=False):
     files = files_to_check.get_files(test_name, MAIN_AMB = MAIN_AMB)
@@ -65,11 +73,11 @@ if __name__ == "__main__":
         clear_sandbox()
         print(" " * 4 + test + " " * (20 - len(test)), end="")
         
-        run_test(test)
+        test_time = run_test(test)
         check = check_files(test, REBUILD_SHA=REBUILD_SHA)
 
         if check == 0:
-            print(TEST_SUCCESS)
+            print(TEST_SUCCESS + " (" + str(round(test_time, 4)) + "s)")
         elif check == 1:
             EXIT_CODE = 1
             print(TEST_FAIL)
