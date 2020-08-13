4e6671a7d89b15f4c8c2018ba14e7326b77520c0
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2020-08-13 17:19:05 +0500

    [TESTS] fixed extract check

--- tests/AMBPatcher/files_to_check.py
+++ tests/AMBPatcher/files_to_check.py
@@ -2,7 +2,7 @@ def get_files(test_name, MAIN_AMB=""):
     tests = {
         "swap_endianness_x2": "add",
         "add_to_swapped": "swap_endianness",
-        "extract": ["#EXCEPT:"+MAIN_AMB],
+        "extract": ["#ALL", "#EXCEPT:"+MAIN_AMB],
         "extract_swapped": "extract",
         "add_from_dir": "add",
 
