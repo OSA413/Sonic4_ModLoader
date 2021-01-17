bd8036311e43d2e98fef4fd785c14972ecd5bec6
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-01-17 21:03:44 +0500

    Alright, it crashes in the built-in method, reinstalling VS

--- Sonic4_ModLoader/AMBPatcher/AMB.cs
+++ Sonic4_ModLoader/AMBPatcher/AMB.cs
@@ -185,70 +185,6 @@ namespace AMB
             return InternalName;
         }
 
-        //FIX THIS LATER
-        public (string InternalName, int InternalIndex, string ParentName, int ParentIndex) GetInternalThings(string OriginalFileName, string ModFileName)
-        {
-            /*
-            * InternalName (like from AMB.Read)
-            * InternalIndex is index of file name in AMB.Read, if present
-            * ParentName is AMB.Read()[ParentIndex].Name, if present
-            * ParentIndex is index of parent file in AMB.Read, if present
-            */
-
-            var files = Objects;
-
-            /////////////////
-            //Internal Name//
-            /////////////////
-
-            string InternalName;
-            int InternalIndex = -1;
-
-            //Turning "C:\1\2\3" into {"C:","1","2","3"}
-            var mod_file_parts = ModFileName.Replace('/', '\\').Split('\\');
-
-            string orig_file_last = Path.GetFileName(OriginalFileName);
-
-            //Trying to find where the original file name starts in the mod file name.
-            int index = Array.IndexOf(mod_file_parts, orig_file_last);
-
-            //If it's inside, return the part after original file ends
-            if (index != -1)
-                InternalName = String.Join("\\", mod_file_parts.Skip(index + 1).ToArray());
-            //Else use file name
-            else
-                InternalName = mod_file_parts.Last();
-
-            //This may occur when main file and added file have the same name
-            if (InternalName == "")
-                InternalName = mod_file_parts.Last();
-
-            //Find internal index
-            for (int i = 0; i < files.Count; i++)
-            {
-                if (InternalName == files[i].Name)
-                { InternalIndex = i; break; }
-            }
-
-            int ParentIndex = -1;
-            string ParentName = "";
-
-            for (int i = 0; i < files.Count; i++)
-            {
-                if ((InternalName + "\\").StartsWith(files[i].Name + "\\"))
-                {
-                    ParentIndex = i;
-                    ParentName = files[i].Name;
-                    break;
-                }
-            }
-
-            return (InternalName: InternalName,
-                    InternalIndex: InternalIndex,
-                    ParentName: ParentName,
-                    ParentIndex: ParentIndex);
-        }
-
         public void Add(string filePath, string newName = null)
         {
             if (newName != null)
@@ -272,7 +208,6 @@ namespace AMB
             if (parent == null) parent = this;
 
             string InternalName;
-            int InternalIndex = -1;
 
             //Turning "C:\1\2\3" into {"C:","1","2","3"}
             var mod_file_parts = objectName.Replace('/', '\\').Split('\\');
@@ -294,27 +229,27 @@ namespace AMB
                 InternalName = mod_file_parts.Last();
 
             //Find internal index
-            for (int i = 0; i < Objects.Count; i++)
-            {
-                if (InternalName == Objects[i].Name)
-                { InternalIndex = i; break; }
-            }
+            int InternalIndex = Objects.FindIndex(x => x.Name == InternalName);
 
             int ParentIndex = -1;
             string ParentName = "";
 
+            //ParentIndex = Objects.FindIndex(x => x.Name == InternalName.Split('\\').First());
             for (int i = 0; i < Objects.Count; i++)
             {
+                var d = InternalName + "\\";
+                var j = d.StartsWith("G_FIX.AMB\\");
+                var a = (InternalName + "\\").StartsWith(Objects[i].Name + "\\");
                 if ((InternalName + "\\").StartsWith(Objects[i].Name + "\\"))
                 {
                     ParentIndex = i;
                     ParentName = Objects[i].Name;
                     break;
                 }
-            }
+            }//*/
 
             if (ParentIndex != -1)
-                return FindObject(ParentName, objectName, parent.Objects[ParentIndex].Amb);
+                return FindObject(Objects[ParentIndex].Name, objectName, parent.Objects[ParentIndex].Amb);
             if (InternalIndex != -1)
                 return parent.Objects[InternalIndex].Amb;
             return null;
