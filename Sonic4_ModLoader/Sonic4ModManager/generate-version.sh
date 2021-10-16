8b3f6f3ffe37e276162359f516e84855c9e90019
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-10-16 23:25:46 +0500

    test again

--- Sonic4_ModLoader/Sonic4ModManager/generate-version.sh
+++ Sonic4_ModLoader/Sonic4ModManager/generate-version.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-echo "Script is running"
 cd "$(dirname "$0")"
 current_commit="$(git rev-parse HEAD)"
-echo "$current_commit"
 version="$(git describe --tags --abbrev=0)"
-echo "$version"
 last_tag_commit="$(git rev-list -n 1 $version)"
-echo "$last_tag_commit"
 
 [ $current_commit != $last_tag_commit ] && version=${current_commit:0:7}
 
