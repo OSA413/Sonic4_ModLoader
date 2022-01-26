efa85683f5ccc8e6d280d74815863d8d26013536
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2022-01-26 23:34:27 +0500

    Now you can build it again on windows

--- build_pack_test.sh
+++ build_pack_test.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 bash Sonic4_ModLoader/generate-version.sh
 
 echo "Compiling..."
-dotnet build Sonic4_ModLoader -c Release -m
+dotnet publish Sonic4_ModLoader -c Release -m
 
 EXIT_CODE="$?"
 if [ "$EXIT_CODE" != "0" ]; then
@@ -21,10 +21,10 @@ echo "Copying new distribution files..."
 #License
 cp "LICENSE" "./dist/Sonic4ModLoader/Mod Loader - licenses/LICENSE-Sonic4_ModLoader"
 #EXEs
-cp "./Sonic4_ModLoader/AMBPatcher/bin/Release/net6.0/AMBPatcher.exe" "./dist/Sonic4ModLoader/AMBPatcher.exe"
-cp "./Sonic4_ModLoader/ManagerLauncher/bin/Release/net6.0-windows/ManagerLauncher.exe" "./dist/Sonic4ModLoader/ManagerLauncher.exe"
-cp "./Sonic4_ModLoader/Sonic4ModManager/bin/Release/net6.0-windows/Sonic4ModManager.exe" "./dist/Sonic4ModLoader/Sonic4ModManager.exe"
-cp "./Sonic4_ModLoader/OneClickModInstaller/bin/Release/net6.0-windows/OneClickModInstaller.exe" "./dist/Sonic4ModLoader/OneClickModInstaller.exe"
+cp "./Sonic4_ModLoader/AMBPatcher/bin/Release/net6.0/win-x64/publish/AMBPatcher.exe" "./dist/Sonic4ModLoader/AMBPatcher.exe"
+cp "./Sonic4_ModLoader/ManagerLauncher/bin/Release/net6.0-windows/publish/ManagerLauncher.exe" "./dist/Sonic4ModLoader/ManagerLauncher.exe"
+cp "./Sonic4_ModLoader/Sonic4ModManager/bin/Release/net6.0-windows/publish/Sonic4ModManager.exe" "./dist/Sonic4ModLoader/Sonic4ModManager.exe"
+cp "./Sonic4_ModLoader/OneClickModInstaller/bin/Release/net6.0-windows/publish/OneClickModInstaller.exe" "./dist/Sonic4ModLoader/OneClickModInstaller.exe"
 #README
 cp "./README.md" "./dist/Sonic4ModLoader/README.md"
 #Change log
