226de92298f84fb907f78aa4e8a2642c5f09392b
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2022-01-30 18:30:49 +0500

    Changed AssemplyInfo

--- Sonic4_ModLoader/Sonic4ModManager/Properties/AssemblyInfo.cs
+++ Sonic4_ModLoader/Sonic4ModManager/Properties/AssemblyInfo.cs
@@ -1,36 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
 [assembly: AssemblyTitle("Sonic4ModManager")]
 [assembly: AssemblyDescription("Mod Manager for Sonic 4")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OSA413")]
 [assembly: AssemblyProduct("Sonic4ModManager")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright ©  2018-2022")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
-
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("8af89a84-4ea0-43c2-9872-25834a7c55e5")]
-
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//   Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
