e587af78433419ed407454cd639fe7fb9d265645
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2022-06-19 14:47:16 +0500

    Update update_dependencies.sh

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -5,12 +5,16 @@ rm -rf "dependencies_source"
 mkdir "dependencies_source"
 cd "dependencies_source"
 
+EXIT_CODE=0
 
 #SonicAudioTools
 git clone --depth=1 https://github.com/blueskythlikesclouds/SonicAudioTools
+[ "$?" != "0" ] && EXIT_CODE=1
 cd "SonicAudioTools"
 nuget restore SonicAudioTools.sln
+[ "$?" != "0" ] && EXIT_CODE=1
 msbuild SonicAudioTools.sln /p:Configuration=Release -m
+[ "$?" != "0" ] && EXIT_CODE=1
 
 cp ./Release/CsbEditor.exe ./../../dependencies/SonicAudioTools/CsbEditor.exe
 cp ./Release/SonicAudioLib.dll ./../../dependencies/SonicAudioTools/SonicAudioLib.dll
@@ -22,8 +26,10 @@ cd ..
 mkdir "7-Zip"
 cd "7-Zip"
 url=$(curl -LIs -w %{url_effective} -o /dev/null https://sourceforge.net/projects/sevenzip/files/latest/download)
+[ "$?" != "0" ] && EXIT_CODE=1
 version=$(basename $(dirname $url))
 curl  $url > 7z_install.exe
+[ "$?" != "0" ] && EXIT_CODE=1
 echo https://sourceforge.net/projects/sevenzip/files/7-Zip/$version > ./../../dependencies/7-Zip/LINK
 
 7z e 7z_install.exe 7z.exe 7z.dll License.txt
@@ -45,3 +51,5 @@ cp -r ./UpdateServer ./../../dependencies/AliceModLoader
 rm ./../../dependencies/AliceModLoader/UpdateServer/update.ini
 
 cd ..
+
+exit $EXIT_CODE
