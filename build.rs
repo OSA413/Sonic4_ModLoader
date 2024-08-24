7b9beb785bb802310fdc12c3a7db38e27aeff794
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2024-08-24 12:33:03 +0500

    Removed example

--- build.rs
+++ build.rs
@@ -1,10 +1,5 @@
 fn main() {
     glib_build_tools::compile_resources(
-        &["src/example/resources"],
-        "src/example/resources/resources.gresource.xml",
-        "actions_6.gresource",
-    );
-    glib_build_tools::compile_resources(
         &["src/ManagerLauncher/resources"],
         "src/ManagerLauncher/resources/resources.gresource.xml",
         "ManagerLauncher.gresource",
