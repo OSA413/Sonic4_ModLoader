d80f041e596c0d9bdeaebe0b98ddf66154c24e90
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2024-09-01 22:04:01 +0500

    Now using Cargo Workspaces

--- rust/ManagerLauncher/build.rs
+++ rust/ManagerLauncher/build.rs
@@ -3,15 +3,15 @@ use winresource::WindowsResource;
 
 fn main() {
     glib_build_tools::compile_resources(
-        &["src/ManagerLauncher/resources"],
-        "src/ManagerLauncher/resources/resources.gresource.xml",
+        &["src/resources"],
+        "src/resources/resources.gresource.xml",
         "ManagerLauncher.gresource",
     );
 
     // TODO: make sure to include the icon only to specified bins
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         WindowsResource::new()
-            .set_icon("icon.ico")
+            .set_icon("../../icon.ico")
             .compile()
             .unwrap();
     }
