052f2fe99d2c45fd8b5cb9cd8196585a805b8b6f
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2024-09-01 21:06:57 +0500

    Update build.rs

--- build.rs
+++ build.rs
@@ -8,6 +8,7 @@ fn main() {
         "ManagerLauncher.gresource",
     );
 
+    // TODO: make sure to include the icon only to specified bins
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         WindowsResource::new()
             .set_icon("icon.ico")
