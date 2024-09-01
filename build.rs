04ea83f7eb2ecac00acfcb29a7e8adaa66890dd3
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2024-09-01 21:06:11 +0500

    Added icon and updated info

--- build.rs
+++ build.rs
@@ -1,7 +1,17 @@
+use std::env;
+use winresource::WindowsResource;
+
 fn main() {
     glib_build_tools::compile_resources(
         &["src/ManagerLauncher/resources"],
         "src/ManagerLauncher/resources/resources.gresource.xml",
         "ManagerLauncher.gresource",
     );
+
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            .set_icon("icon.ico")
+            .compile()
+            .unwrap();
+    }
 }
