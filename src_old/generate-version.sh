b74e9d463f8f55f1e3d9518ced2b44655cebe518
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2025-09-28 18:50:54 +0500

    Remove old Mod Manager

--- src_old/generate-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-current_commit="$(git rev-parse HEAD)"
-version="$(git describe --tags --abbrev=0)"
-[ $? = 0 ] && last_tag_commit="$(git rev-list -n 1 $version)" || last_tag_commit="0"
-
-[ $current_commit != $last_tag_commit ] && version=${current_commit:0:7}
-
-echo $version > version
