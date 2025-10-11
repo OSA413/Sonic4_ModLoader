ba9a4cb6d9a2e350647ad404edf2a6494a5ddec7
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2025-10-11 23:18:35 +0500

    JSON name fixes

--- src/amb-rs/src/amb_management.rs
+++ src/amb-rs/src/amb_management.rs
@@ -48,7 +48,7 @@ pub fn get_json_string_amb_table_of_content(source: Vec<u8>, name: String) -> St
     let amb = Amb::new_from_src_ptr_name(&source, Some(0), name);
     let mut amb_toc = Vec::<String>::new();
 
-    amb_toc.push(add_json_entry_str("name", &amb.amb_path));
+    amb_toc.push(add_json_entry_str("name", &amb.amb_path.replace("\\", "\\\\")));
     let amb_version = match &amb.version { Version::PC => &"PC".to_string(), Version::Mobile => &"Mobile".to_string() };
     amb_toc.push(add_json_entry_str("version", amb_version));
     let amb_endianness = match &amb.endianness { 
@@ -60,8 +60,8 @@ pub fn get_json_string_amb_table_of_content(source: Vec<u8>, name: String) -> St
     let mut objects_toc = Vec::<String>::new();
     for binary_object in amb.objects {
         let mut object_toc = Vec::<String>::new();
-        object_toc.push(add_json_entry_str("name", &binary_object.name));
-        object_toc.push(add_json_entry_str("real_name", &binary_object.real_name));
+        object_toc.push(add_json_entry_str("name", &binary_object.name.replace("\\", "\\\\")));
+        object_toc.push(add_json_entry_str("real_name", &binary_object.real_name.replace("\\", "\\\\")));
         object_toc.push(add_json_entry("flag1", &binary_object.flag1.to_string()));
         object_toc.push(add_json_entry("flag2", &binary_object.flag2.to_string()));
         object_toc.push(add_json_entry("pointer", &binary_object.pointer.to_string()));
