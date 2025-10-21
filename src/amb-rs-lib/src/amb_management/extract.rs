f4af91805330f70ad4c5f6a014bf11752894231d
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2025-10-21 21:45:15 +0500

    Fix extraction of AMB

--- src/amb-rs-lib/src/amb_management/extract.rs
+++ src/amb-rs-lib/src/amb_management/extract.rs
@@ -7,6 +7,7 @@ pub fn extract_amb(target_file: String, dir_to_extract: Option<String>) {
         Ok(amb) => {
             let base_dir = dir_to_extract.unwrap_or(format!("{}_extracted", &target_file));
             let base_dir = Path::new(&base_dir);
+            fs::create_dir_all(base_dir).unwrap();
             for binary_object in amb.objects {
                 match fs::write(base_dir.join(&binary_object.name), &binary_object.data) {
                     Ok(_) => println!("Extracted {}", &binary_object.name),
