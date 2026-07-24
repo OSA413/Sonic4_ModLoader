db68126af0e04db768e8057979e4c0cbd345dfda
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2026-07-24 20:00:52 +0500

    refactor: use CARGO_PKG_REPOSITORY for repository url

--- src/Sonic4FilePatcher/src/help.rs
+++ src/Sonic4FilePatcher/src/help.rs
@@ -1,5 +1,9 @@
-pub fn get_text() -> &'static str {
-"Usage:
+pub fn print() {
+    println!("        Sonic4FilePatcher by OSA413
+        Released under the MIT License
+        {}
+
+Usage:
     `Sonic4FilePatcher`
         If \"mods\" directory exists, patch all files used by enabled mods, else show this help message.
 
@@ -12,9 +16,5 @@ pub fn get_text() -> &'static str {
 
     `Sonic4FilePatcher -h` and
     `Sonic4FilePatcher --help`
-        Show this help message."
-}
-
-pub fn print() {
-    println!("{}", get_text());
+        Show this help message.", env!("CARGO_PKG_REPOSITORY"));
 }
